43f62458e75e6a0a208a5dab58c90186953c2429
liangxiaojun <fatalxiao@163.com>
2021-07-29 11:28:00 +0800

    update index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+export interface VivyRouterPluginOption {
+
+    /**
+     * history of Vivy router plugin to handle router
+     */
+    history?: Object
+
+}
+
+export default function createVivyRouterPlugin(options?: VivyRouterPluginOption);
