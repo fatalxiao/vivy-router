cd9478ab4b9a14e679d552b22de9f35c670b9a74
liangxiaojun <fatalxiao@163.com>
2022-02-19 14:26:42 +0800

    update unit test

---
+++ test/VivyRouter.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import Vivy from 'vivy';
+import VivyRouter from '../src';
+
+import {createMemoryHistory} from 'history';
+
+test('Use Vivy Router by memory history', () => {
+
+    const history = createMemoryHistory();
+    const vivy = Vivy();
+
+    vivy.use(VivyRouter({
+        history
+    }));
+
+    const store = vivy.createStore();
+
+    expect(
+        store.history
+    ).toEqual(
+        history
+    );
+
+});
