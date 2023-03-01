8481e95287638b87d66df4c3c2f0488a5580b2ca
liangxiaojun <fatalxiao@163.com>
2023-03-01 16:23:33 +0800

    update example

--- examples/withRouterPlugin/src/modules/Root/containers/Root.js
+++ examples/withRouterPlugin/src/modules/Root/containers/Root.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-vivy';
+import {useModelState} from 'react-vivy';
 
 // Component
 import {NavLink} from 'react-router-dom';
@@ -12,49 +12,55 @@ import {NavLink} from 'react-router-dom';
 // Vendors
 import {renderRoutes} from 'react-router-config';
 
-// Styles
-import './Root.scss';
-
 const Root = ({
-    route,
-    menu
-}) => (
-    <div className="root">
+    route
+}) => {
 
-        <div className="menu">
-            <h2>Module Root</h2>
-            <h3>Menu:</h3>
-            <ul>
-                {
-                    menu?.map((item, index) =>
-                        <li key={index}>
-                            <NavLink to={item?.route}>
-                                {item?.name}
-                            </NavLink>
-                        </li>
-                    )
-                }
-            </ul>
+    /**
+     * Get state from model using hook "useModelState".
+     */
+    const {menu} = useModelState('root');
+
+    return (
+        <div style={{
+            display: 'flex',
+            boxSizing: 'border-box',
+            height: '100vh'
+        }}>
+
+            <div style={{
+                padding: 24,
+                background: '#f0f0f0'
+            }}>
+                <h2>Module Root</h2>
+                <h3>Menu:</h3>
+                <ul>
+                    {
+                        menu?.map((item, index) =>
+                            <li key={index}>
+                                <NavLink to={item?.route}>
+                                    {item?.name}
+                                </NavLink>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+
+            <div style={{
+                flex: 1,
+                padding: 24
+            }}>
+                {renderRoutes(route.routes)}
+            </div>
+
         </div>
-
-        <div className="content">
-            {renderRoutes(route.routes)}
-        </div>
-
-    </div>
-);
-
-Root.propTypes = {
-
-    route: PropTypes.object,
-
-    menu: PropTypes.array
+    );
 
 };
 
-export default connect(state => ({
+Root.propTypes = {
+    route: PropTypes.object
+};
 
-    // Get menu config from root model
-    menu: state.root.menu
-
-}))(Root);
+export default Root;
